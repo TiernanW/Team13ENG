a53c2ea85efd2d0ef589349088b78e8d44564771
TiernanW <zcabtwa@ucl.ac.uk>
2017-10-23 15:15:38 +0100

    Columns Adjusted
    Made more mobile compatible by adding media queries

---
+++ js/carousel.js
@@ -0,0 +1,26 @@
+var slide = 0;
+var slides = document.getElementsByClassName("carSlide");
+			
+function upSlide() {
+	slide += 1;
+	if (slide > slides.length - 1) {
+		slide = 0;
+	}
+	changeSlide(slide);
+}
+
+function downSlide() {
+	slide -= 1;
+	if (slide < 0) {
+		slide = slides.length - 1;
+	}
+	changeSlide(slide);
+}
+
+function changeSlide(n) {
+	var i;
+	for(i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[n].style.display = "block";
+}
