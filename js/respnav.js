ffa1c82b541459fc9b5553bd640c32c17d92e658
TiernanW <zcabtwa@ucl.ac.uk>
2017-11-05 13:52:23 +0000

    Redesigned Intro

--- js/respnav.js
+++ js/respnav.js
@@ -1,21 +1,30 @@
-var others = document.getElementById("nav").getElementsByTagName("a");
 var nav = document.getElementById("nav");
+var arefs = document.getElementById("nav").getElementsByTagName("a");
 document.getElementById("menuButton").addEventListener("click", displayMenu);
+
+var topB = document.getElementById("top");
+
+function colorNav() {
+	var offset = window.pageYOffset;
+	if(offset > 80) {
+		topB.style.backgroundColor = "#fff";
+		for(i = 0; i < arefs.length; i++) {
+			arefs[i].style.color = "#222";
+		}
+	} else {
+		topB.style.backgroundColor = "transparent";
+		for(i = 0; i < arefs.length; i++) {
+			arefs[i].style.color = "#fff";
+		}
+	}
+}
+
+window.addEventListener('scroll', colorNav);
 
 function displayMenu() {
 	if(nav.className == "topNav") {
 		nav.className += " resp";
 	} else {
 		nav.className = "topNav";
-	}
-}
-
-function displayMenu2() {
-	for(i = 0; i < 5; i++) {
-		if(others[i].className == "other-link") {
-			others[i].className += "resp";
-		} else {
-			others[i].className = "other-link";
-		}
 	}
 }
